8b48432ad74725fd66918a15977538d2d26cf812
Kalluri Laxmi Narashimha Lokesh Kumar <95397474+klnlokeshkumar@users.noreply.github.com>
2024-06-18 08:11:42 +0530

    upto pig game

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,3 +1,33 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
 
+///////////////////////////////////////
+// Coding Challenge #1
+
+/*
+Given an array of forecasted maximum temperatures, the thermometer displays a string with these temperatures.
+
+Example: [17, 21, 23] will print "... 17ºC in 1 days ... 21ºC in 2 days ... 23ºC in 3 days ..."
+
+Create a function 'printForecast' which takes in an array 'arr' and logs a string like the above to the console.
+
+Use the problem-solving framework: Understand the problem and break it up into sub-problems!
+
+TEST DATA 1: [17, 21, 23]
+TEST DATA 2: [12, 5, -5, 0, 4]
+*/
+
+
+
+const printForecast = function (temps){
+    let forcastMessage = "... ";
+    for(let i = 0; i < temps.length; i++){
+        forcastMessage += `${temps[i]}oC in ${i+1} days ... `;
+    }
+    return forcastMessage;
+}
+
+let temps = [17, 21, 23];
+console.log(printForecast(temps));
+temps = [12, 5, -5, 0, 4];
+console.log(printForecast(temps));
--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,57 @@
 'use strict';
+
+// console.log(document.querySelector(".message").textContent);
+// document.querySelector(".message").textContent = "correct number!";
+// document.querySelector(".number").textContent = 13;
+// document.querySelector(".score").textContent = 10;
+// document.querySelector(".guess").value = 8
+// console.log(document.querySelector(".guess").value);
+
+let secretNumber = Math.trunc(Math.random()*20) + 1;
+// console.log(secretNumber);
+let guessedNumber = Number(document.querySelector(".guess").value);
+let score = 20;
+let highscore = 0;
+
+const changeMessage = function(identifier, message){
+    document.querySelector(identifier).textContent = message;
+}
+
+document.querySelector(".check").addEventListener("click", function(){
+    if(score > 0){
+        guessedNumber = Number(document.querySelector(".guess").value);
+        if(!guessedNumber){
+            changeMessage(".message", "Pls enter a number first...");
+        }
+        else if(guessedNumber === secretNumber){
+            changeMessage(".message", "Correct answer '''");
+            document.querySelector("body").style.backgroundColor = "green";
+            document.querySelector(".number").style.width = "30rem";
+            changeMessage(".number", secretNumber);
+            if(highscore < score){
+                highscore = score;
+                changeMessage(".highscore", highscore);
+            }
+        }
+        else {
+            changeMessage(".message", guessedNumber > secretNumber ? "Too high >>>" : "Too Low <<<");
+            score--;
+            changeMessage(".score", score);
+        }
+    }
+    else {
+        changeMessage(".message", "You lost the game ~~ start again");
+    }
+})
+
+document.querySelector(".again").addEventListener("click", function(){
+    changeMessage(".message", "give it a try again..");
+    document.querySelector("body").style.backgroundColor = "#222222";
+    document.querySelector(".number").style.width = "15rem";
+    changeMessage(".number", "?");
+    document.querySelector('.guess').value = '';
+    changeMessage(".score", "20");
+    score = 20;
+    secretNumber = secretNumber = Math.trunc(Math.random()*20) + 1;
+    console.log(secretNumber);
+})
