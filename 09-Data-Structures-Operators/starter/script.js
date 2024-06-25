d9a4db09d9a5228a66af1303b0b81279d8e42268
Kalluri Laxmi Narashimha Lokesh Kumar <95397474+klnlokeshkumar@users.noreply.github.com>
2024-06-25 22:12:29 +0530

    upto 11 arrays

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -26,4 +26,151 @@ const restaurant = {
       close: 24,
     },
   },
+  orderFood: function (starteIndex, mainIndex) {
+    return [this.starterMenu[starteIndex], this.mainMenu[mainIndex]];
+  },
+
+  orderDelivery: function (obj) {
+    // we can use default parameter instead of obj
+    console.log(obj);
+  },
 };
+
+// const newMenu = [...restaurant.mainMenu];
+// console.log(newMenu);
+
+// restaurant.orderDelivery({
+//   time: '22:30',
+//   address: 'cia jdfhas, 21',
+//   mainIndex: 2,
+//   starteIndex: 2,
+// });
+
+// const { name, openingHours, categories } = restaurant;
+// console.log(name, openingHours, categories);
+
+// const {
+//   name: restaurantName,
+//   openingHours: hours,
+//   categories: tags,
+// } = restaurant;
+// console.log(restaurantName, tags, hours);
+
+// const { menu = [], starterMenu: starters = [] } = restaurant;
+// console.log(menu, starters);
+
+// let a = 123;
+// let b = 888;
+// const obj = { a: 23, b: 7, c: 14 };
+
+// ({ a, b } = obj);
+// console.log(a, b);
+
+// // Nested objects
+// const { fri } = openingHours;
+// console.log(fri);
+
+// const arr = [2, 3, 4];
+// const a = arr[0];
+// const b = arr[1];
+// const c = arr[2];
+
+// console.log(a, b, c);
+
+// const [x, y, z] = arr;
+// console.log(x, y, z);
+// console.log(arr);
+
+// // const [first, second] = restaurant.categories;
+// let [first, , second] = restaurant.categories;
+// console.log(first, second);
+
+// without destructuring
+// const temp = first;
+// first =  second;
+// second = temp;
+// console.log(first, second);
+
+// with destructuring
+// [first, second] = [second, first];
+// console.log(first, second);
+
+// [first, second] = restaurant.orderFood(0, 2);
+// console.log(first, second);
+
+// const nested = [2, 3, [5, 6]];
+// const [i, , [j, k]] = nested;
+// console.log(i, j, k);
+
+// const tempArr = [8];
+// const [p, q, r] = tempArr;
+// console.log(p, q, r);
+
+// const [s = 1, t = 2] = tempArr;
+// console.log(s, t);
+
+const game = {
+  team1: 'Bayern Munich',
+  team2: 'Borrussia Dortmund',
+  players: [
+    [
+      'Neuer',
+      'Pavard',
+      'Martinez',
+      'Alaba',
+      'Davies',
+      'Kimmich',
+      'Goretzka',
+      'Coman',
+      'Muller',
+      'Gnarby',
+      'Lewandowski',
+    ],
+    [
+      'Burki',
+      'Schulz',
+      'Hummels',
+      'Akanji',
+      'Hakimi',
+      'Weigl',
+      'Witsel',
+      'Hazard',
+      'Brandt',
+      'Sancho',
+      'Gotze',
+    ],
+  ],
+  score: '4:0',
+  scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+  date: 'Nov 9th, 2037',
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
+  },
+};
+
+const [players1, players2] = game.players;
+console.log(players1, players2);
+
+const [gk, ...fieldPlayers] = players1;
+console.log(gk, fieldPlayers);
+
+const allPlayers = [...players1, ...players2];
+console.log(allPlayers);
+
+const players1Final = [...players1, 'thiago', 'constinue', 'jsadf'];
+console.log(players1Final);
+
+const {
+  odds: { team1, x: draw, team2 },
+} = game;
+console.log(team1, draw, team2);
+
+const printGoals = function (...players) {
+  console.log(`${players.length} goals were scored`);
+};
+printGoals(...game.scored);
+
+team1 < team2 && console.log('Team1 is more likely to win');
+team2 < team1 && console.log('Team2 is more likely to win');
--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -61,6 +61,43 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
+const displayMovements = function (movements) {
+  containerMovements.innerHTML = '';
+
+  movements.forEach(function (mov, i) {
+    const type = mov > 0 ? 'deposit' : 'withdrawl';
+
+    const html = `
+        <div class="movements__row">
+          <div class="movements__type movements__type--deposit"> ${
+            i + 1
+          } ${type}</div>
+          <div class="movements__value">${mov}€</div>
+        </div>
+      `;
+
+    containerMovements.insertAdjacentHTML('afterbegin', html);
+  });
+};
+displayMovements(account1.movements);
+
+const createUsernames = function (accs) {
+  accs.forEach(function (acc) {
+    acc.username = acc.owner
+      .toLowerCase()
+      .split(' ')
+      .map(function (name) {
+        return name[0];
+      })
+      .join('');
+  });
+};
+createUsernames(accounts);
+console.log(accounts);
+
+// console.log(username);
+
+// console.log(containerMovements.innerHTML);
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
